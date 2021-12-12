5adf011ae2d0bc9679f8731694dec0afcc1d8509
Brian <BMArsenault@gmail.com>
2021-12-11 23:57:32 -0700

    Updated employee testing

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,25 @@
 const Employee = require('../lib/Employee');
 const Index = require('../index');
+const { TestWatcher } = require('jest');
+
+jest.mock('../lib/Employee.js');
+
+test('creates a new employee object', () => {
+    const employee = new Employee('Brian');
+    expect(employee.name).toBe('Brian');
+})
+
+test('creates a new ID object', () => {
+    const employeeId = new EmployeeID('22');
+    expect(employee.id).toBe('22');
+})
+
+test('creates a new employee object', () => {
+    const employeeEmail = new EmployeeEmail('Brian');
+    expect(employee.email).toBe('Brian');
+})
+
+test('creates a new "employee" role', () => {
+    const employeeRole = new EmployeeRole('Employee');
+    expect(employee.role).toBe('Employee');
+})
